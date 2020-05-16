6cc15d22f6c9a4e82597ace5f0ed800c4e0edd7e
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-16 10:14:34 -0500

    Add suppressions file

---
+++ SharpRSS.FeedParser/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Kalen Dubrick">
+// Copyright (c) Kalen Dubrick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "Reviewed")]
