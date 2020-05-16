e3bd3602e3bcc1d1798bb8de831e15468a01b8cf
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-16 10:15:33 -0500

    Add intial feed objects
    They are based solely on RSS 2.0 for the moment.

---
+++ SharpRSS.FeedParser/Models/FeedImage.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="FeedImage.cs" company="Kalen Dubrick">
+// Copyright (c) Kalen Dubrick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SharpRSS.FeedParser.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents an image in a feed.
+    /// </summary>
+    public class FeedImage
+    {
+        #region Required Elements
+
+        /// <summary>
+        /// Gets or sets the URL for the image.
+        /// </summary>
+        public Uri Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title for the image.
+        /// Note: in practice, often the same value as <see cref="Feed.Title"/>.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the link for the image.
+        /// Note: in practice, often the same value as <see cref="Feed.Link"/>.
+        /// </summary>
+        public Uri Link { get; set; }
+
+        #endregion
+
+        #region Optional Elements
+
+        /// <summary>
+        /// Gets or sets the width of the image, in pixels.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height of the image, in pixels.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description for the image.
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+    }
+}
---
+++ SharpRSS.FeedParser/Models/FeedTextInput.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="FeedTextInput.cs" company="Kalen Dubrick">
+// Copyright (c) Kalen Dubrick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SharpRSS.FeedParser.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents a text input elemnent for channel.
+    /// </summary>
+    public class FeedTextInput
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the text object
+        /// in the text input area.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the CGI script that
+        /// processes text input requests.
+        /// </summary>
+        public Uri Link { get; set; }
+    }
+}
