52c14bf302cdfab82c74517733ad4eb58cd874e1
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-23 08:41:24 -0500

    Flesh out FeedItemEnclosure

--- SharpRSS.FeedParser/Models/FeedItemEnclosure.cs
+++ SharpRSS.FeedParser/Models/FeedItemEnclosure.cs
@@ -5,10 +5,26 @@
 
 namespace SharpRSS.FeedParser.Models
 {
+    using System;
+
     /// <summary>
     /// Represents an enclosure in a feed item.
     /// </summary>
     public class FeedItemEnclosure
     {
+        /// <summary>
+        /// Gets or sets the URL where the enclosure is located.
+        /// </summary>
+        public Uri Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the length of the enclosure, in bytes.
+        /// </summary>
+        public long Length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the MIME type of the enclosure.
+        /// </summary>
+        public string MimeType { get; set; }
     }
 }
