f27fc5b6d4ff842e0197bfe6904aad09c010df69
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-27 07:59:56 -0500

    Remove TestCaseSource attributes for now
    Also cleaned up usings

--- SharpRSS.FeedParser.Tests/FeedCloudValidatorTests.cs
+++ SharpRSS.FeedParser.Tests/FeedCloudValidatorTests.cs
@@ -1,38 +1,12 @@
 namespace SharpRSS.FeedParser.Tests
 {
-    using System.Threading.Tasks;
-
-    using FluentValidation;
-    using FluentValidation.Results;
     using FluentValidation.TestHelper;
     using NUnit.Framework;
     using SharpRSS.FeedParser.Models;
     using SharpRSS.FeedParser.Validators;
-    
+
     public class FeedCloudValidatorTests
     {
-        private static FeedCloud[] validFeedCloudCases = 
-        {
-            new FeedCloud()
-            {
-                Domain = "rpc.sys.com",
-                Port = 80,
-                Path = "/RPC2",
-                RegisterProcedure = "myCloud.rssPleaseNotify",
-                Protocol = "xml-rpc"
-            }
-        };
-        private static FeedCloud[] invalidFeedCloudCases =
-        {
-            new FeedCloud()
-            {
-                Domain = "",
-                Port = 80,
-                Path = "/RPC2",
-                RegisterProcedure = "myCloud.rssPleaseNotify",
-                Protocol = "xml-rpc"
-            }
-        };
         private FeedCloudValidator validator;
 
         [SetUp]
@@ -41,19 +15,35 @@ namespace SharpRSS.FeedParser.Tests
             validator = new FeedCloudValidator();
         }
 
-        [TestCaseSource("validFeedCloudCases")]
         [Test]
-        public void ValidFeedCloudShouldNotReturnValidatorErrors(FeedCloud feedCloud)
+        public void ValidFeedCloudShouldNotReturnValidatorErrors()
         {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = 80,
+                Path = "/RPC2",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = "xml-rpc"
+            };
+
             var result = validator.TestValidate(feedCloud);
 
             result.ShouldNotHaveAnyValidationErrors();
         }
 
-        [TestCaseSource("invalidFeedCloudCases")]
         [Test]
-        public void EmptyDomainShouldReturnValidatorError(FeedCloud feedCloud)
+        public void EmptyDomainShouldReturnValidatorError()
         {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "",
+                Port = 80,
+                Path = "/RPC2",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = "xml-rpc"
+            };
+
             var result = validator.TestValidate(feedCloud);
 
             result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Domain);
