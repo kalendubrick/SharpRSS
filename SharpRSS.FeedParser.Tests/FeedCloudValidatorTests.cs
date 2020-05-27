3aa35f4656ac9f435c13cd5faf983b7d5fff1dff
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-26 19:52:41 -0500

    Finish FeedCloudValidator tests

--- SharpRSS.FeedParser.Tests/FeedCloudValidatorTests.cs
+++ SharpRSS.FeedParser.Tests/FeedCloudValidatorTests.cs
@@ -58,5 +58,90 @@ namespace SharpRSS.FeedParser.Tests
 
             result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Domain);
         }
+
+        [Test]
+        public void PortLessThanMinShouldReturnValidatorError()
+        {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = -1,
+                Path = "/RPC2",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = "xml-rpc"
+            };
+
+            var result = validator.TestValidate(feedCloud);
+
+            result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Port);
+        }
+
+        [Test]
+        public void PortGreaterThanMaxShouldReturnValidatorError()
+        {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = 80000,
+                Path = "/RPC2",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = "xml-rpc"
+            };
+
+            var result = validator.TestValidate(feedCloud);
+
+            result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Port);
+        }
+
+        [Test]
+        public void EmptyPathShouldReturnValidatorError()
+        {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = 80,
+                Path = "",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = "xml-rpc"
+            };
+
+            var result = validator.TestValidate(feedCloud);
+
+            result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Path);
+        }
+
+        [Test]
+        public void EmptyRegisterProcedureShouldReturnValidatorError()
+        {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = 80,
+                Path = "/RPC2",
+                RegisterProcedure = "",
+                Protocol = "xml-rpc"
+            };
+
+            var result = validator.TestValidate(feedCloud);
+
+            result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.RegisterProcedure);
+        }
+
+        [Test]
+        public void EmptyProtocolShouldReturnValidatorError()
+        {
+            var feedCloud = new FeedCloud()
+            {
+                Domain = "rpc.sys.com",
+                Port = 80,
+                Path = "/RPC2",
+                RegisterProcedure = "myCloud.rssPleaseNotify",
+                Protocol = ""
+            };
+
+            var result = validator.TestValidate(feedCloud);
+
+            result.ShouldHaveValidationErrorFor(feedCloud => feedCloud.Protocol);
+        }
     }
 }
