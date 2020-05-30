56caf639bb326f4f616b544f93120d4808c2e15f
Kalen Dubrick <kalen@kalendubrick.com>
2020-05-29 20:52:58 -0500

    Bring URL validators in line with others

--- SharpRSS.FeedParser/Validators/FeedItemValidator.cs
+++ SharpRSS.FeedParser/Validators/FeedItemValidator.cs
@@ -28,12 +28,14 @@ namespace SharpRSS.FeedParser.Validators
                 .NotEmpty()
                 .When(feed => string.IsNullOrEmpty(feed.Title));
             this.RuleFor(feedItem => feedItem.Link)
+                .NotNull()
                 .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
-                .When(feedItem => !string.IsNullOrWhiteSpace(feedItem.Link));
+                .When(feedItem => !string.IsNullOrWhiteSpace(feedItem.Link), ApplyConditionTo.CurrentValidator);
             this.RuleForEach(feedItem => feedItem.Categories).NotEmpty();
             this.RuleFor(feedItem => feedItem.Comments)
+                .NotNull()
                 .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
-                .When(feedItem => !string.IsNullOrWhiteSpace(feedItem.Comments));
+                .When(feedItem => !string.IsNullOrWhiteSpace(feedItem.Comments), ApplyConditionTo.CurrentValidator);
 
             // Validators from sub-elements
             this.RuleFor(feedItem => feedItem.Author).SetValidator(new FeedPersonValidator());
