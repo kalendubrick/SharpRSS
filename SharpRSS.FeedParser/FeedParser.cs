e6d6b5916d10ae1510d8a2de76ad8c6dfffcc46b
Kalen Dubrick <kalen@kalendubrick.com>
2020-06-07 17:27:58 -0500

    Add additional fields to parser

--- SharpRSS.FeedParser/FeedParser.cs
+++ SharpRSS.FeedParser/FeedParser.cs
@@ -6,6 +6,7 @@
 namespace SharpRSS.FeedParser
 {
     using System;
+    using System.Globalization;
     using System.Runtime.CompilerServices;
     using System.Xml;
     using SharpRSS.FeedParser.Models;
@@ -65,6 +66,8 @@ namespace SharpRSS.FeedParser
 
             var feedElement = root.FirstChild as XmlElement;
             var feed = new Feed();
+            var dateFmt = new CultureInfo("en-us").DateTimeFormat;
+            var numFmt = new CultureInfo("en-us").NumberFormat;
 
             foreach (XmlElement child in feedElement.ChildNodes)
             {
@@ -79,10 +82,92 @@ namespace SharpRSS.FeedParser
                     case "description":
                         feed.Description = child.InnerText;
                         break;
+                    case "language":
+                        feed.Language = child.InnerText;
+                        break;
+                    case "copyright":
+                        feed.Copyright = child.InnerText;
+                        break;
+                    case "managingEditor":
+                        feed.ManagingEditor = new FeedPerson() { EmailAddress = child.InnerText };
+                        break;
+                    case "webmaster":
+                        feed.Webmaster = new FeedPerson() { EmailAddress = child.InnerText };
+                        break;
+                    case "publishDate":
+                        if (feed.Language != null)
+                        {
+                            dateFmt = new CultureInfo(feed.Language).DateTimeFormat;
+                        }
+
+                        feed.PublishDate = DateTimeOffset.Parse(child.InnerText, dateFmt);
+                        break;
+                    case "lastBuildDate":
+                        if (feed.Language != null)
+                        {
+                            dateFmt = new CultureInfo(feed.Language).DateTimeFormat;
+                        }
+
+                        feed.LastBuildDate = DateTimeOffset.Parse(child.InnerText, dateFmt);
+                        break;
+                    case "category":
+                        feed.Categories.Add(child.InnerText);
+                        break;
+                    case "generator":
+                        feed.Generator = child.InnerText;
+                        break;
+                    case "docs":
+                        feed.Docs = child.InnerText;
+                        break;
+                    case "cloud":
+                        if (feed.Language != null)
+                        {
+                            numFmt = new CultureInfo(feed.Language).NumberFormat;
+                        }
+
+                        feed.Cloud = this.ParseFeedCloud(child, numFmt);
+                        break;
                 }
             }
 
             return feed;
+        }
+
+        private FeedCloud ParseFeedCloud(XmlElement el, NumberFormatInfo fmt)
+        {
+            var feedCloud = new FeedCloud();
+
+            foreach (XmlAttribute attr in el.Attributes)
+            {
+                switch (attr.Name)
+                {
+                    case "domain":
+                        feedCloud.Domain = attr.Value;
+                        break;
+                    case "port":
+                        try
+                        {
+                            feedCloud.Port = int.Parse(attr.Value, fmt);
+                        }
+                        catch (FormatException)
+                        {
+                            feedCloud.Port = 0;
+                        }
+
+                        break;
+                    case "path":
+                        feedCloud.Path = attr.Value;
+                        break;
+                    case "registerProcedure":
+                        feedCloud.RegisterProcedure = attr.Value;
+                        break;
+                    case "protocol":
+                        feedCloud.Protocol = attr.Value;
+                        break;
+                }
+            }
+
+            return feedCloud;
         }
     }
 }
