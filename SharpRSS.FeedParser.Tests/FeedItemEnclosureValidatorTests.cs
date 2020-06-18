0f6df1b23251bd0ddec6c79b8aaf104acc87e44a
Kalen Dubrick <kalen@kalendubrick.com>
2020-06-18 08:30:16 -0500

    Start test class for FeedItemEnclosureValidator

---
+++ SharpRSS.FeedParser.Tests/FeedItemEnclosureValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SharpRSS.FeedParser.Tests
+{
+    using FluentValidation.TestHelper;
+    using NUnit.Framework;
+    using SharpRSS.FeedParser.Models;
+    using SharpRSS.FeedParser.Validators;
+
+    class FeedItemEnclosureValidatorTests
+    {
+        private FeedItemEnclosureValidator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new FeedItemEnclosureValidator();
+        }
+
+        [Test]
+        public void ValidFeedItemEnclosureShouldNotReturnValidatorErrors()
+        {
+            var enclosure = new FeedItemEnclosure()
+            {
+                Url = "http://www.scripting.com/mp3s/weatherReportSuite.mp3",
+                Length = 12216320,
+                MimeType = "audio/mpeg"
+            };
+
+            var result = validator.TestValidate(enclosure);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
