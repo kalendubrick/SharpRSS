879c4a8b694436f272c72bf218dbb05f22026a21
Kalen Dubrick <kalen@kalendubrick.com>
2020-06-18 08:29:48 -0500

    Add missing validator for FeedItemEnclosure

---
+++ SharpRSS.FeedParser/Validators/FeedItemEnclosureValidator.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="FeedItemEnclosureValidator.cs" company="Kalen Dubrick">
+// Copyright (c) Kalen Dubrick. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SharpRSS.FeedParser.Validators
+{
+    using System;
+
+    using FluentValidation;
+    using SharpRSS.FeedParser.Models;
+
+    /// <summary>
+    /// Validator for the <see cref="FeedItemEnclosure"/> class.
+    /// </summary>
+    public class FeedItemEnclosureValidator : AbstractValidator<FeedItemEnclosure>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedItemEnclosureValidator"/> class.
+        /// </summary>
+        public FeedItemEnclosureValidator()
+        {
+            this.RuleFor(feedItemEnclosure => feedItemEnclosure.Url)
+                .NotEmpty()
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+                .When(feedItemEnclosure => !string.IsNullOrWhiteSpace(feedItemEnclosure.Url), ApplyConditionTo.CurrentValidator);
+            this.RuleFor(feedItemEnclousure => feedItemEnclousure.Length).NotEmpty();
+            this.RuleFor(feedItemEnclosure => feedItemEnclosure.MimeType).NotEmpty();
+        }
+    }
+}
