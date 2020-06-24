f9c8f3c3abcbce33707add3cd4e6eb2fac6051d6
Kalen Dubrick <kalen@kalendubrick.com>
2020-06-23 19:32:46 -0500

    Finish FeedItemValidator test methods

--- SharpRSS.FeedParser.Tests/FeedItemEnclosureValidatorTests.cs
+++ SharpRSS.FeedParser.Tests/FeedItemEnclosureValidatorTests.cs
@@ -29,5 +29,50 @@
 
             result.ShouldNotHaveAnyValidationErrors();
         }
+
+        [Test]
+        public void InvalidFeedItemEnclosureUrlShouldReturnValidatorError()
+        {
+            var enclosure = new FeedItemEnclosure()
+            {
+                Url = "http:/.mp3",
+                Length = 12216320,
+                MimeType = "audio/mpeg"
+            };
+
+            var result = validator.TestValidate(enclosure);
+
+            result.ShouldHaveValidationErrorFor(enclosure => enclosure.Url);
+        }
+
+        [Test]
+        public void InvalidFeedItemEnclosureLengthShouldReturnValidatorError()
+        {
+            var enclosure = new FeedItemEnclosure()
+            {
+                Url = "http://www.scripting.com/mp3s/weatherReportSuite.mp3",
+                Length = 0,
+                MimeType = "audio/mpeg"
+            };
+
+            var result = validator.TestValidate(enclosure);
+
+            result.ShouldHaveValidationErrorFor(enclosure => enclosure.Length);
+        }
+
+        [Test]
+        public void InvalidFeedItemEnclosureMimeTypeShouldReturnValidatorError()
+        {
+            var enclosure = new FeedItemEnclosure()
+            {
+                Url = "http://www.scripting.com/mp3s/weatherReportSuite.mp3",
+                Length = 12216320,
+                MimeType = null
+            };
+
+            var result = validator.TestValidate(enclosure);
+
+            result.ShouldHaveValidationErrorFor(enclosure => enclosure.MimeType);
+        }
     }
 }
